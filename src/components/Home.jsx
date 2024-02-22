7fbfc57547d39c76dcfd25e051ed46a3594a6f79
Vijay-CPP <vijaytamada02@gmail.com>
2024-02-22 19:31:29 +0530

    Ready to deploy

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import doctor from "../assets/hero.jpg"
 
 const Home = () => {
   return (
-    <div className='bg-hero h-screen bg-center bg-cover sm:bg-fixed'>
+    <div className='hero-bg h-screen bg-center bg-cover sm:bg-fixed'>
       <Navbar/>
       <div className='container mx-auto w-screen h-screen flex items-center'>
         <Advertisement/>
